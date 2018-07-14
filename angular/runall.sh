00349e9e9c22c0bfb70e9e03eb95acb631b55d8b
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2018-07-14 21:27:50 +0300

    Added angular cli and folder to the shell container.

--- angular/runall.sh
+++ angular/runall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Running /data/crm_schools/httpdocs_angular/ "
+echo "Running /data/crm_schools/httpdocs_angular/"
 cd "/data/crm_schools/httpdocs_angular/"
 npm install
 npm start
